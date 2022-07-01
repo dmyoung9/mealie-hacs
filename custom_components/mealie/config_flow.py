044f8139ea28ac262d68a48d075ffc47d006e5f6
David Young <dmyoung9@asu.edu>
2022-07-01 15:18:35 -0700

    :fire: - Remove integration options

--- custom_components/mealie/config_flow.py
+++ custom_components/mealie/config_flow.py
@@ -45,10 +45,10 @@ class MealieFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
 
         return await self._show_config_form(user_input)
 
-    @staticmethod
-    @callback
-    def async_get_options_flow(config_entry):
-        return MealieOptionsFlowHandler(config_entry)
+    # @staticmethod
+    # @callback
+    # def async_get_options_flow(config_entry):
+    #     return MealieOptionsFlowHandler(config_entry)
 
     async def _show_config_form(self, user_input):  # pylint: disable=unused-argument
         """Show the configuration form to edit location data."""
@@ -76,36 +76,36 @@ class MealieFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
         return False
 
 
-class MealieOptionsFlowHandler(config_entries.OptionsFlow):
-    """Config flow options handler for mealie."""
+# class MealieOptionsFlowHandler(config_entries.OptionsFlow):
+#     """Config flow options handler for mealie."""
 
-    def __init__(self, config_entry):
-        """Initialize HACS options flow."""
-        self.config_entry = config_entry
-        self.options = dict(config_entry.options)
+#     def __init__(self, config_entry):
+#         """Initialize HACS options flow."""
+#         self.config_entry = config_entry
+#         self.options = dict(config_entry.options)
 
-    async def async_step_init(self, user_input=None):  # pylint: disable=unused-argument
-        """Manage the options."""
-        return await self.async_step_user()
+#     async def async_step_init(self, user_input=None):  # pylint: disable=unused-argument
+#         """Manage the options."""
+#         return await self.async_step_user()
 
-    async def async_step_user(self, user_input=None):
-        """Handle a flow initialized by the user."""
-        if user_input is not None:
-            self.options.update(user_input)
-            return await self._update_options()
+#     async def async_step_user(self, user_input=None):
+#         """Handle a flow initialized by the user."""
+#         if user_input is not None:
+#             self.options.update(user_input)
+#             return await self._update_options()
 
-        return self.async_show_form(
-            step_id="user",
-            data_schema=vol.Schema(
-                {
-                    vol.Required(x, default=self.options.get(x, True)): bool
-                    for x in sorted(PLATFORMS)
-                }
-            ),
-        )
+#         return self.async_show_form(
+#             step_id="user",
+#             data_schema=vol.Schema(
+#                 {
+#                     vol.Required(x, default=self.options.get(x, True)): bool
+#                     for x in sorted(PLATFORMS)
+#                 }
+#             ),
+#         )
 
-    async def _update_options(self):
-        """Update config entry options."""
-        return self.async_create_entry(
-            title=self.config_entry.data.get(CONF_USERNAME), data=self.options
-        )
+#     async def _update_options(self):
+#         """Update config entry options."""
+#         return self.async_create_entry(
+#             title=self.config_entry.data.get(CONF_USERNAME), data=self.options
+#         )
