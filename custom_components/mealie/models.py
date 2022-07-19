06b7bcd2f1acbc0410fc84aa146ccc7040c48b78
Tom Matheussen <tommatheussen5@hotmail.com>
2022-07-19 14:06:05 +0000

    Add basic models

---
+++ custom_components/mealie/models.py
@@ -0,0 +1,50 @@
+from typing import Optional, List
+from pydantic import BaseModel
+from homeassistant.backports.enum import StrEnum
+
+
+"""Mealie API objects"""
+
+
+class EntryTypes(StrEnum):
+    """Enum to represent the entry types."""
+
+    BREAKFAST = "breakfast"
+    LUNCH = "lunch"
+    DINNER = "dinner"
+    SIDE = "side"
+
+
+class Recipe(BaseModel):
+    """Recipe model."""
+
+    name: str
+    slug: str
+
+
+class MealPlan(BaseModel):
+    """Meal plan model."""
+
+    date: str
+    entryType: str
+    title: Optional[str]
+    text: Optional[str]
+    recipeId: Optional[str]
+    recipe: Optional[Recipe]
+
+
+class About(BaseModel):
+    """About model."""
+
+    version: str
+    # TODO: add configuration URL here?
+
+
+class MealieData:
+    """Mealie API data."""
+
+    def __init__(self):
+        self.mealPlans = []
+
+    mealPlans: list[MealPlan]
+    about: About
